24c23fcc336d7c4d94fd1153076d037a46608a0e
daccola <78117344+daccola@users.noreply.github.com>
2022-07-06 17:09:11 -0500

    skeleton logic

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
 exports.handler = async (event) => {
+  const data = {
+    version: "1.0",
+    time: "",
+    teamInfo: [],
+
+    nfcEastStandings: [],
+    nfcNorthStandings: [],
+    nfcSouthStandings: [],
+    nfcWestStandings: [],
+
+    nfcDivisionChamps: [],
+    nfcWildCardTeams: [],
+
+       
+    afcEastStandings: [],
+    afcNorthStandings: [],
+    afcSouthStandings: [],
+    afcWestStandings: [],
+
+    afcDivisionChamps: [],
+    afcWildCardTeams: [],
+ 
+  }
+
+  console.log('$$$$$$$$$')
+  console.log(JSON.stringify(data))
+  console.log('$$$$$$$$$')
+
   const response = {
     statusCode: 200,
     body: JSON.stringify("Hello from Lambda and Github!"),
