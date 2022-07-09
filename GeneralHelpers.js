f80e56f9a006a50b6020c77df6fbf066bdb23bab
daccola <78117344+daccola@users.noreply.github.com>
2022-07-09 17:58:50 -0500

    split into multiple files

---
+++ GeneralHelpers.js
@@ -0,0 +1,411 @@
+export function getDefaultTeamInfo() {
+    const teams = {}
+  
+      teams['ARI'] = {
+          location: 'Arizona',
+          abbreviation: 'ARI',
+          alternateName: null,
+          conference: 'NFC',
+          division: 'West',
+          overallRecord: [0, 0, 0],
+          conferenceRecord: [0, 0, 0],
+          divisionRecord: [0, 0, 0],
+          games: []
+      }
+  
+      teams['ATL'] = {
+          location: 'Atlanta',
+          abbreviation: 'ATL',
+          alternateName: null,
+          conference: 'NFC',
+          division: 'South',
+          overallRecord: [0, 0, 0],
+          conferenceRecord: [0, 0, 0],
+          divisionRecord: [0, 0, 0],
+          games: []
+      }
+  
+      teams['BAL'] = {
+          location: 'Baltimore',
+          abbreviation: 'BAL',
+          alternateName: null,
+          conference: 'AFC',
+          division: 'North',
+          overallRecord: [0, 0, 0],
+          conferenceRecord: [0, 0, 0],
+          divisionRecord: [0, 0, 0],
+          games: []
+      }
+  
+      teams['BUF'] = {
+          location: 'Buffalo',
+          abbreviation: 'BUF',
+          alternateName: null,
+          conference: 'AFC',
+          division: 'East',
+          overallRecord: [0, 0, 0],
+          conferenceRecord: [0, 0, 0],
+          divisionRecord: [0, 0, 0],
+          games: []
+      }
+  
+      teams['CAR'] = {
+          location: 'Carolina',
+          abbreviation: 'CAR',
+          alternateName: null,
+          conference: 'NFC',
+          division: 'South',
+          overallRecord: [0, 0, 0],
+          conferenceRecord: [0, 0, 0],
+          divisionRecord: [0, 0, 0],
+          games: []
+      }
+  
+      teams['CHI'] = {
+          location: 'Chicago',
+          abbreviation: 'CHI',
+          alternateName: null,
+          conference: 'NFC',
+          division: 'North',
+          overallRecord: [0, 0, 0],
+          conferenceRecord: [0, 0, 0],
+          divisionRecord: [0, 0, 0],
+          games: []
+      }
+  
+      teams['CIN'] = {
+          location: 'Cincinnati',
+          abbreviation: 'CIN',
+          alternateName: null,
+          conference: 'AFC',
+          division: 'North',
+          overallRecord: [0, 0, 0],
+          conferenceRecord: [0, 0, 0],
+          divisionRecord: [0, 0, 0],
+          games: []
+      }
+  
+      teams['CLE'] = {
+          location: 'Cleveland',
+          abbreviation: 'CLE',
+          alternateName: null,
+          conference: 'AFC',
+          division: 'North',
+          overallRecord: [0, 0, 0],
+          conferenceRecord: [0, 0, 0],
+          divisionRecord: [0, 0, 0],
+          games: []
+      }
+  
+      teams['DAL'] = {
+          location: 'Dallas',
+          abbreviation: 'DAL',
+          alternateName: null,
+          conference: 'NFC',
+          division: 'East',
+          overallRecord: [0, 0, 0],
+          conferenceRecord: [0, 0, 0],
+          divisionRecord: [0, 0, 0],
+          games: []
+      }
+  
+      teams['DEN'] = {
+          location: 'Denver',
+          abbreviation: 'DEN',
+          alternateName: null,
+          conference: 'AFC',
+          division: 'West',
+          overallRecord: [0, 0, 0],
+          conferenceRecord: [0, 0, 0],
+          divisionRecord: [0, 0, 0],
+          games: []
+      }
+  
+      teams['DET'] = {
+          location: 'Detroit',
+          abbreviation: 'DET',
+          alternateName: null,
+          conference: 'NFC',
+          division: 'North',
+          overallRecord: [0, 0, 0],
+          conferenceRecord: [0, 0, 0],
+          divisionRecord: [0, 0, 0],
+          games: []
+      }
+  
+      teams['GB'] = {
+          location: 'Green Bay',
+          abbreviation: 'GB',
+          alternateName: null,
+          conference: 'NFC',
+          division: 'North',
+          overallRecord: [0, 0, 0],
+          conferenceRecord: [0, 0, 0],
+          divisionRecord: [0, 0, 0],
+          games: []
+      }
+  
+      teams['HOU'] = {
+          location: 'Houston',
+          abbreviation: 'HOU',
+          alternateName: null,
+          conference: 'AFC',
+          division: 'South',
+          overallRecord: [0, 0, 0],
+          conferenceRecord: [0, 0, 0],
+          divisionRecord: [0, 0, 0],
+          games: []
+      }
+  
+      teams['IND'] = {
+          location: 'Indianapolis',
+          abbreviation: 'IND',
+          alternateName: null,
+          conference: 'AFC',
+          division: 'South',
+          overallRecord: [0, 0, 0],
+          conferenceRecord: [0, 0, 0],
+          divisionRecord: [0, 0, 0],
+          games: []
+      }
+  
+      teams['JAX'] = {
+          location: 'Jacksonville',
+          abbreviation: 'JAX',
+          alternateName: null,
+          conference: 'AFC',
+          division: 'South',
+          overallRecord: [0, 0, 0],
+          conferenceRecord: [0, 0, 0],
+          divisionRecord: [0, 0, 0],
+          games: []
+      }
+  
+      teams['KC'] = {
+          location: 'Kansas City',
+          abbreviation: 'KC',
+          alternateName: null,
+          conference: 'AFC',
+          division: 'West',
+          overallRecord: [0, 0, 0],
+          conferenceRecord: [0, 0, 0],
+          divisionRecord: [0, 0, 0],
+          games: []
+      }
+  
+      teams['LV'] = {
+          location: 'Las Vegas',
+          abbreviation: 'LV',
+          alternateName: null,
+          conference: 'AFC',
+          division: 'West',
+          overallRecord: [0, 0, 0],
+          conferenceRecord: [0, 0, 0],
+          divisionRecord: [0, 0, 0],
+          games: []
+      }
+  
+      teams['LAC'] = {
+          location: 'Los Angeles',
+          abbreviation: 'LAC',
+          alternateName: 'LA Chargers',
+          conference: 'AFC',
+          division: 'West',
+          overallRecord: [0, 0, 0],
+          conferenceRecord: [0, 0, 0],
+          divisionRecord: [0, 0, 0],
+          games: []
+      }
+  
+      teams['LAR'] = {
+          location: 'Los Angeles',
+          abbreviation: 'LAR',
+          alternateName: 'LA Rams',
+          conference: 'NFC',
+          division: 'West',
+          overallRecord: [0, 0, 0],
+          conferenceRecord: [0, 0, 0],
+          divisionRecord: [0, 0, 0],
+          games: []
+      }
+  
+      teams['MIA'] = {
+          location: 'Miami',
+          abbreviation: 'MIA',
+          alternateName: null,
+          conference: 'AFC',
+          division: 'East',
+          overallRecord: [0, 0, 0],
+          conferenceRecord: [0, 0, 0],
+          divisionRecord: [0, 0, 0],
+          games: []
+      }
+  
+      teams['MIN'] = {
+          location: 'Minnesota',
+          abbreviation: 'MIN',
+          alternateName: null,
+          conference: 'NFC',
+          division: 'North',
+          overallRecord: [0, 0, 0],
+          conferenceRecord: [0, 0, 0],
+          divisionRecord: [0, 0, 0],
+          games: []
+      }
+  
+      teams['NE'] = {
+          location: 'New England',
+          abbreviation: 'NE',
+          alternateName: null,
+          conference: 'AFC',
+          division: 'East',
+          overallRecord: [0, 0, 0],
+          conferenceRecord: [0, 0, 0],
+          divisionRecord: [0, 0, 0],
+          games: []
+      }
+  
+      teams['NO'] = {
+          location: 'New Orleans',
+          abbreviation: 'NO',
+          alternateName: null,
+          conference: 'NFC',
+          division: 'South',
+          overallRecord: [0, 0, 0],
+          conferenceRecord: [0, 0, 0],
+          divisionRecord: [0, 0, 0],
+          games: []
+      }
+  
+      teams['NYG'] = {
+          location: 'New York',
+          abbreviation: 'NYG',
+          alternateName: "NY Giants",
+          conference: 'NFC',
+          division: 'East',
+          overallRecord: [0, 0, 0],
+          conferenceRecord: [0, 0, 0],
+          divisionRecord: [0, 0, 0],
+          games: []
+      }
+  
+      teams['NYJ'] = {
+          location: 'New York',
+          abbreviation: 'NYJ',
+          alternateName: 'NY Jets',
+          conference: 'AFC',
+          division: 'East',
+          overallRecord: [0, 0, 0],
+          conferenceRecord: [0, 0, 0],
+          divisionRecord: [0, 0, 0],
+          games: []
+      }
+  
+      teams['PHI'] = {
+          location: 'Philadelphia',
+          abbreviation: 'PHI',
+          alternateName: null,
+          conference: 'NFC',
+          division: 'East',
+          overallRecord: [0, 0, 0],
+          conferenceRecord: [0, 0, 0],
+          divisionRecord: [0, 0, 0],
+          games: []
+      }
+  
+      teams['PIT'] = {
+          location: 'Pittsburgh',
+          abbreviation: 'PIT',
+          alternateName: null,
+          conference: 'AFC',
+          division: 'North',
+          overallRecord: [0, 0, 0],
+          conferenceRecord: [0, 0, 0],
+          divisionRecord: [0, 0, 0],
+          games: []
+      }
+  
+      teams['SF'] = {
+          location: 'San Francisco',
+          abbreviation: 'SF',
+          alternateName: null,
+          conference: 'NFC',
+          division: 'West',
+          overallRecord: [0, 0, 0],
+          conferenceRecord: [0, 0, 0],
+          divisionRecord: [0, 0, 0],
+          games: []
+      }
+  
+      teams['SEA'] = {
+          location: 'Seattle',
+          abbreviation: 'SEA',
+          alternateName: null,
+          conference: 'NFC',
+          division: 'West',
+          overallRecord: [0, 0, 0],
+          conferenceRecord: [0, 0, 0],
+          divisionRecord: [0, 0, 0],
+          games: []
+      }
+  
+      teams['TB'] = {
+          location: 'Tampa Bay',
+          abbreviation: 'TB',
+          alternateName: null,
+          conference: 'NFC',
+          division: 'South',
+          overallRecord: [0, 0, 0],
+          conferenceRecord: [0, 0, 0],
+          divisionRecord: [0, 0, 0],
+          games: []
+      }
+  
+      teams['TEN'] = {
+          location: 'Tennessee',
+          abbreviation: 'TEN',
+          alternateName: null,
+          conference: 'AFC',
+          division: 'South',
+          overallRecord: [0, 0, 0],
+          conferenceRecord: [0, 0, 0],
+          divisionRecord: [0, 0, 0],
+          games: []
+      }
+  
+      teams['WSH'] = {
+          location: 'Washington',
+          abbreviation: 'WSH',
+          alternateName: null,
+          conference: 'NFC',
+          division: 'East',
+          overallRecord: [0, 0, 0],
+          conferenceRecord: [0, 0, 0],
+          divisionRecord: [0, 0, 0],
+          games: []
+      }
+  
+      return teams
+  }
+
+export function sortOnlyByRecord(teams){
+  const sorted = teams.sort(function (x, y) {
+    var diff = getWonLostTiedPercentage(y.overallRecord) - getWonLostTiedPercentage(x.overallRecord)
+    return diff
+  })
+
+  return sorted
+}
+
+export function getWonLostTiedPercentage(record){
+	const totalGames = record[0] + record[1] + record[2]
+	if(totalGames === 0) {
+		return 0
+	}
+	return (record[0] + (record[2] * 0.500)) / totalGames
+}
+
+export function sortCompletely(divisionTeams){
+	//TODO
+	return divisionTeams
+}
