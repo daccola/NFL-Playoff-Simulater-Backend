73149e09f22fd4531a860bf85defb404106ddd9a
daccola <78117344+daccola@users.noreply.github.com>
2022-07-09 19:21:49 -0500

    add everything back

--- index.js
+++ index.js
@@ -1,75 +1,59 @@
 import { getUpdatedTeamInfo } from './TeamInfoHelpers.js'
 import { getTeamsByDivision, getDivisionStandings } from './DivisionHelpers.js'
-//getTeamsByDivision
-//getDivisionStandings
-//const https = require('https')
-import https from 'https'
 
 export const handler = async () => {
-
-//exports.handler = async (event) => {
-  //const teamInfo = getDefaultTeamInfo()
   const teamInfo = await getUpdatedTeamInfo('2021')
+	
+  const nfcEastTeams = getTeamsByDivision('NFC', 'East', teamInfo)
+  const nfcEastTeamsSorted = getDivisionStandings(nfcEastTeams)
+  
+  const nfcNorthTeams = getTeamsByDivision('NFC', 'North', teamInfo)
+  const nfcNorthTeamsSorted = getDivisionStandings(nfcNorthTeams)
+  
+  const nfcSouthTeams = getTeamsByDivision('NFC', 'South', teamInfo)
+  const nfcSouthTeamsSorted = getDivisionStandings(nfcSouthTeams)
+  
+  const nfcWestTeams = getTeamsByDivision('NFC', 'West', teamInfo)
+  const nfcWestTeamsSorted = getDivisionStandings(nfcWestTeams)
+  
+  const afcEastTeams = getTeamsByDivision('AFC', 'East', teamInfo)
+  const afcEastTeamsSorted = getDivisionStandings(afcEastTeams)
+  
+  const afcNorthTeams = getTeamsByDivision('AFC', 'North', teamInfo)
+  const afcNorthTeamsSorted = getDivisionStandings(afcNorthTeams)
+  
+  const afcSouthTeams = getTeamsByDivision('AFC', 'South', teamInfo)
+  const afcSouthTeamsSorted = getDivisionStandings(afcSouthTeams)
+  
+  const afcWestTeams = getTeamsByDivision('AFC', 'West', teamInfo)
+  const afcWestTeamsSorted = getDivisionStandings(afcWestTeams)
+  
+  const allData = {
+    version: "1.0",
+    time: "",
+    teamInfoToDate: teamInfo,
 
-//const year = '2021'
+    nfcEastStandings: nfcEastTeamsSorted,
+    nfcNorthStandings: nfcNorthTeamsSorted,
+    nfcSouthStandings: nfcSouthTeamsSorted,
+    nfcWestStandings: nfcWestTeamsSorted,
 
-	//Gets game info for all 18 weeks
-  
-		//end for loop
-  //}
-  
-  // const nfcEastTeams = getTeamsByDivision('NFC', 'East', teamInfo)
-  // const nfcEastTeamsSorted = getDivisionStandings(nfcEastTeams)
-  
-  // const nfcNorthTeams = getTeamsByDivision('NFC', 'North', teamInfo)
-  // const nfcNorthTeamsSorted = getDivisionStandings(nfcNorthTeams)
-  
-  // const nfcSouthTeams = getTeamsByDivision('NFC', 'South', teamInfo)
-  // const nfcSouthTeamsSorted = getDivisionStandings(nfcSouthTeams)
-  
-  // const nfcWestTeams = getTeamsByDivision('NFC', 'West', teamInfo)
-  // const nfcWestTeamsSorted = getDivisionStandings(nfcWestTeams)
-  
-  // const afcEastTeams = getTeamsByDivision('AFC', 'East', teamInfo)
-  // const afcEastTeamsSorted = getDivisionStandings(afcEastTeams)
-  
-  // const afcNorthTeams = getTeamsByDivision('AFC', 'North', teamInfo)
-  // const afcNorthTeamsSorted = getDivisionStandings(afcNorthTeams)
-  
-  // const afcSouthTeams = getTeamsByDivision('AFC', 'South', teamInfo)
-  // const afcSouthTeamsSorted = getDivisionStandings(afcSouthTeams)
-  
-  // const afcWestTeams = getTeamsByDivision('AFC', 'West', teamInfo)
-  // const afcWestTeamsSorted = getDivisionStandings(afcWestTeams)
-  
-  // const allData = {
-  //   version: "1.0",
-  //   time: "",
-  //   teamInfoToDate: teamInfo,
+    nfcDivisionChamps: [],
+    nfcWildCardTeams: [],
 
-  //   nfcEastStandings: nfcEastTeamsSorted,
-  //   nfcNorthStandings: nfcNorthTeamsSorted,
-  //   nfcSouthStandings: nfcSouthTeamsSorted,
-  //   nfcWestStandings: nfcWestTeamsSorted,
+    afcEastStandings: afcEastTeamsSorted,
+    afcNorthStandings: afcNorthTeamsSorted,
+    afcSouthStandings: afcSouthTeamsSorted,
+    afcWestStandings: afcWestTeamsSorted,
 
-  //   nfcDivisionChamps: [],
-  //   nfcWildCardTeams: [],
-
-  //   afcEastStandings: afcEastTeamsSorted,
-  //   afcNorthStandings: afcNorthTeamsSorted,
-  //   afcSouthStandings: afcSouthTeamsSorted,
-  //   afcWestStandings: afcWestTeamsSorted,
-
-  //   afcDivisionChamps: [],
-  //   afcWildCardTeams: [],
-  // }
+    afcDivisionChamps: [],
+    afcWildCardTeams: [],
+  }
   
-  // const response = {
-  //   statusCode: 200,
-  //   body: JSON.stringify(allData),
-  // }
-  //  //return getWonLostTiedPercentage(allData.teamInfoToDate['GB'].overallRecord)
-  // return response
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(allData),
+  }
 
-	return JSON.stringify(teamInfo)
+  return response
 }
